2ed74a9e1d9ebedc8782a1c499502075b65c3ab6
zhaoq0201 <zhaoq0201@163.com>
2023-07-17 11:33:44 +0800

    feat: retry exponential

--- retry/retry.go
+++ retry/retry.go
@@ -1,16 +1,1 @@
-package main
-
-import (
-	"fmt"
-	"math"
-	"time"
-)
-
-func main() {
-	backoff := NewExponentialBackoffManager(time.Second, time.Minute, time.Minute*2, math.MaxInt32, 2.0, 1.0, clock.RealClock{})
-	fmt.Println("-->", time.Now())
-	stopCh := make(chan struct{})
-	BackoffUtil(func() {
-		fmt.Println("-->", time.Now())
-	}, backoff, true, stopCh)
-}
+package retry
